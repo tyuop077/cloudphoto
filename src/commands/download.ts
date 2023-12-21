3f665fdf1918e9349224925daea65a52e7e8b779
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2023-12-21 04:03:09 +0300

    Updated download command

--- src/commands/download.ts
+++ src/commands/download.ts
@@ -3,33 +3,44 @@ import { Readable } from "stream";
 import { pipeline } from "node:stream/promises";
 import fs from "fs";
 import path from "path";
+import { getDirectory } from "../utils/directory.js";
+import { readConfig } from "../utils/config.js";
 
-export default async function download(options: { album: string; path?: string }) {
-  const directory = options.path ?? process.cwd();
-  const s3Client = new S3Client({ region: "ru-central1a" });
+export default async function download(options: { album?: string; path?: string }) {
+  if (!options.album) throw new Error("Album was not provided");
 
-  const params = {
-    Bucket: process.env.BUCKET_NAME,
-    Prefix: `${options.album}/`,
-  };
+  const directory = getDirectory(options.path);
+  const config = readConfig();
 
-  const data = await s3Client.send(new ListObjectsV2Command(params));
+  const s3Client = new S3Client({ region: config.region });
 
-  if (!data.Contents) throw new Error("TODO");
+  const data = await s3Client.send(
+    new ListObjectsV2Command({
+      Bucket: config.bucket,
+      Prefix: `${options.album}/`,
+    })
+  );
 
-  data.Contents.forEach(async file => {
+  if (!data.Contents) throw new Error("No images found or album doesn't exist");
+
+  for (const file of data.Contents) {
     if (!file.Key) return;
 
-    const getParams = {
-      Bucket: process.env.BUCKET_NAME,
-      Key: file.Key,
-    };
-    const response = await s3Client.send(new GetObjectCommand(getParams));
+    const response = await s3Client.send(
+      new GetObjectCommand({
+        Bucket: process.env.BUCKET_NAME,
+        Key: file.Key,
+      })
+    );
 
     if (!(response.Body instanceof Readable)) return;
 
     const fileStream = fs.createWriteStream(directory + "/" + path.basename(file.Key));
 
-    await pipeline(response.Body, fileStream);
-  });
+    try {
+      await pipeline(response.Body, fileStream);
+    } catch (err) {
+      throw new Error(`Cannot write ${file.Key} into directory: ${(<Error>err).message ?? err}`);
+    }
+  }
 }
