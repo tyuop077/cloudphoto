d30ed9b1628f83578ea7401a018473007e1a63db
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2023-12-21 04:12:59 +0300

    Updated delete command

--- src/commands/delete.ts
+++ src/commands/delete.ts
@@ -1,16 +1,30 @@
 import { S3Client, DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { readConfig } from "../utils/config.js";
 
-export default async function remove(options: { album: string; photo?: string }) {
-  const s3Client = new S3Client({ region: "ru-central1a" });
-  const params = {
-    Bucket: process.env.BUCKET_NAME,
-    Key: options.photo ? `${options.album}/${options.photo}` : `${options.album}/`,
-  };
+export default async function remove(options: { album?: string; photo?: string }) {
+  if (!options.album) throw new Error("Album was not provided");
 
+  const config = readConfig();
+
+  const s3Client = new S3Client({ region: config.region });
   try {
-    await s3Client.send(new DeleteObjectCommand(params));
+    await s3Client.send(
+      new DeleteObjectCommand({
+        Bucket: config.bucket,
+        Key: options.photo ? `${options.album}/${options.photo}` : `${options.album}/`,
+      })
+    );
     console.log("Success");
   } catch (err) {
-    console.log("Error", err);
+    if (options.photo) {
+      throw new Error("Image was not found");
+    } else {
+      await s3Client.send(
+        new DeleteObjectCommand({
+          Bucket: config.bucket,
+          Key: `${options.album}`,
+        })
+      );
+    }
   }
 }
