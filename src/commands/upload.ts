8cdf1b4ab6f89df033b6339d2841dde75437a57f
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2023-12-21 03:47:32 +0300

    Added config handle and made file iteration async in upload command

--- src/commands/upload.ts
+++ src/commands/upload.ts
@@ -1,27 +1,35 @@
 import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
 import fs from "fs";
-import { glob } from "glob";
+import { Glob } from "glob";
 import path from "path";
+import { readConfig } from "../utils/config.js";
 
-export default async function upload(options: { album: string; path?: string }) {
-  const s3Client = new S3Client({ region: "ru-central1a" });
+export default async function upload(options: { album?: string; path?: string }) {
+  if (!options.album) throw new Error("Album was not provided");
   const directory = options.path ?? process.cwd();
-  const images = glob.sync(directory + "/*.jpg");
 
-  for (const image of images) {
-    const fileStream = fs.createReadStream(image);
+  if (!fs.existsSync(directory)) {
+    throw new Error(`Directory does not exist: ${directory}`);
+  }
 
-    const params = {
-      Bucket: process.env.BUCKET_NAME,
-      Key: `${options.album}/${path.basename(image)}`,
-      Body: fileStream,
-    };
+  const config = readConfig();
+
+  const s3Client = new S3Client({ region: "ru-central1a" });
+  const imageNames = new Glob(directory + "/*.{png,jpg,jpeg,webm}", {});
+
+  for await (const imageName of imageNames) {
+    const fileStream = fs.createReadStream(imageName);
 
     try {
-      const data = await s3Client.send(new PutObjectCommand(params));
-      console.log(`Updated ${path.basename(image)}`);
+      const data = await s3Client.send(
+        new PutObjectCommand({
+          Bucket: config.bucket,
+          Key: `${options.album}/${path.basename(imageName)}`,
+          Body: fileStream,
+        })
+      );
     } catch (err) {
-      console.log("Error", err);
+      console.warn(`Cannot upload ${imageName}: ${(<Error>err).message ?? err}`);
     }
   }
 }
