f8af602bebe2e9a2928b1420fc4353077ddedc5a
tyuop077 <30123136+tyuop077@users.noreply.github.com>
2023-12-21 07:11:51 +0300

    Finished mksite command

--- src/commands/mksite.ts
+++ src/commands/mksite.ts
@@ -3,6 +3,7 @@ import { readConfig } from "../utils/config.js";
 import BucketClient from "../utils/bucketClient.js";
 import { groupByAlbum } from "../utils/groupByAlbum.js";
 import { uploadHTML } from "../utils/uploadHTML.js";
+import { onlyFolders } from "../utils/onlyFolders.js";
 
 export default async function mksite() {
   const config = readConfig();
@@ -18,7 +19,10 @@ export default async function mksite() {
 
   if (!dataList.Contents) throw new Error(`No bucket contents found`);
 
-  const albums = groupByAlbum(dataList.Contents.map(file => <string>file.Key).filter(Boolean));
+  const albums = groupByAlbum(onlyFolders(dataList.Contents.map(file => <string>file.Key).filter(Boolean)));
+
+  console.log(dataList.Contents.map(file => <string>file.Key).filter(Boolean));
+  console.log(albums);
 
   const index_html = `<!doctype html>
 <html>
@@ -41,6 +45,38 @@ ${Object.keys(albums)
     client: bucketClient,
   });
 
-  /*for (const [album, fileNames] of Object.entries(albums)) {
-  }*/
+  let i = 1;
+
+  for (const [album, fileNames] of Object.entries(albums)) {
+    const album_html = `<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/galleria/1.6.1/themes/classic/galleria.classic.min.css" />
+        <style>
+            .galleria{ width: 960px; height: 540px; background: #000 }
+        </style>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/galleria/1.6.1/galleria.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/galleria/1.6.1/themes/classic/galleria.classic.min.js"></script>
+    </head>
+    <body>
+        <div class="galleria">
+${fileNames.map(fileName => `            <img src="${album}/${fileName}" data-title="${fileName}">`).join("\n")}
+        </div>
+        <p>Вернуться на <a href="index.html">главную страницу</a> фотоархива</p>
+        <script>
+            (function() {
+                Galleria.run('.galleria');
+            }());
+        </script>
+    </body>
+</html>`;
+
+    await uploadHTML({
+      bucket: config.bucket,
+      filename: `album${i++}.html`,
+      content: album_html,
+      client: bucketClient,
+    });
+  }
 }
